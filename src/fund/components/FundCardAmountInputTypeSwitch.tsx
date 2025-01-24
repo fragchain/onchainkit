5bb337c1d045d2ea69164f4870e7b89476fc36fd
Brendan Forster <brendan.forster@coinbase.com>
2025-01-24 14:33:20 -0800

    dedupe utils

--- src/fund/components/FundCardAmountInputTypeSwitch.tsx
+++ src/fund/components/FundCardAmountInputTypeSwitch.tsx
@@ -1,10 +1,10 @@
 import { formatFiatAmount } from '@/internal/utils/formatFiatAmount';
+import { truncateDecimalPlaces } from '@/internal/utils/truncateDecimalPlaces';
 import { useCallback, useMemo } from 'react';
 import { useIcon } from '../../core-react/internal/hooks/useIcon';
 import { Skeleton } from '../../internal/components/Skeleton';
 import { cn, pressable, text } from '../../styles/theme';
 import type { FundCardAmountInputTypeSwitchPropsReact } from '../types';
-import { truncateDecimalPlaces } from '../utils/truncateDecimalPlaces';
 import { useFundContext } from './FundCardProvider';
 
 export const FundCardAmountInputTypeSwitch = ({
